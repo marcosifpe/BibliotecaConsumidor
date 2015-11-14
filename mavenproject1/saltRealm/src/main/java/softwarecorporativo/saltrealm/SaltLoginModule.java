e769351293b9611c08d9bcd169da116474b521e8
marcosifpe <marcosifpe@gmail.com>
2015-11-13 23:18:14 -0300

    Atualização.

--- mavenproject1/saltRealm/src/main/java/softwarecorporativo/saltrealm/SaltLoginModule.java
+++ mavenproject1/saltRealm/src/main/java/softwarecorporativo/saltrealm/SaltLoginModule.java
@@ -5,109 +5,32 @@
  */
 package softwarecorporativo.saltrealm;
 
-import com.sun.appserv.connectors.internal.api.ConnectorRuntime;
 import com.sun.appserv.security.AppservPasswordLoginModule;
-import com.sun.enterprise.security.common.Util;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
-import javax.naming.NamingException;
 import javax.security.auth.login.LoginException;
-import javax.sql.DataSource;
-import org.glassfish.hk2.api.ActiveDescriptor;
-import org.glassfish.hk2.utilities.BuilderHelper;
 
 /**
  *
  * @author MASC
  */
 public class SaltLoginModule extends AppservPasswordLoginModule {
-    private static DataSource dataSource;
 
-    private synchronized Connection getConnection() throws NamingException, SQLException {
-        if (dataSource == null) {
-            SaltRealm realm = (SaltRealm) _currentRealm;
-            ActiveDescriptor<ConnectorRuntime> cr = (ActiveDescriptor<ConnectorRuntime>) Util.getDefaultHabitat().getBestDescriptor(BuilderHelper.createContractFilter(ConnectorRuntime.class.getName()));
-            ConnectorRuntime connectorRuntime = Util.getDefaultHabitat().getServiceHandle(cr).getService();
-            dataSource = (DataSource) connectorRuntime.lookupNonTxResource(realm.getJtaDataSource(), false);
-        }
-        
-        return dataSource.getConnection();
-    }
-
-    public String getHash(String salt) {
-        try {
-            SaltRealm realm = (SaltRealm) _currentRealm;
-            String pwd = salt + _password;
-            MessageDigest digest = MessageDigest.getInstance(realm.getHashAlgorithm());
-            digest.update(pwd.getBytes(Charset.forName(realm.getCharset())));
-            return Base64.getEncoder().encodeToString(digest.digest());
-        } catch (NoSuchAlgorithmException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-    
-    
     @Override
     protected void authenticateUser() throws LoginException {
-        Connection conn = null;
         SaltRealm realm = (SaltRealm) _currentRealm;
-        try {
-            conn = getConnection();
-            PreparedStatement stmt = conn.prepareStatement(realm.getPasswordQuery());
-            stmt.setString(1, _username);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                String password = rs.getString(1);      
-                String salt = rs.getString(2);
-
-                if (password == null || !password.equals(getHash(salt))) {
-                    throw new LoginException("Invalid login. Please try again");
-                }
-            }
-
-            rs.close();
-            stmt.close();
-
-            stmt = conn.prepareStatement(realm.getGroupsQuery());
-            stmt.setString(1, _username);
-            rs = stmt.executeQuery();
-            
-            List<String> groups = new ArrayList<>();
-            while (rs.next()) {
-                String group = rs.getString(1);
-                groups.add(group);
-            }
-            
-            String[] groupsArray = new String[groups.size()];
+        if (realm.authenticateUser(_username, _password)) {
+            List<String> groupsList = realm.getGroupList(_username);
+            String[] groups = new String[groupsList.size()];
             int i = 0;
-            for (String group : groups) {
-                groupsArray[i++] = group;
+            for (String group : groupsList) {
+                groups[i++] = group;
             }
 
-            rs.close();
-            stmt.close();
-            commitUserAuthentication(groupsArray);            
-        } catch (SQLException | NamingException ex) {
-            throw new RuntimeException(ex.getMessage(), ex);
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex.getMessage(), ex);
-            }
+            commitUserAuthentication(groups);
+        } else {
+            throw new LoginException("Invalid login!");
         }
-        
+
     }
-    
+
 }
--- mavenproject1/saltRealm/src/main/java/softwarecorporativo/saltrealm/SaltRealm.java
+++ mavenproject1/saltRealm/src/main/java/softwarecorporativo/saltrealm/SaltRealm.java
@@ -1,15 +1,29 @@
 package softwarecorporativo.saltrealm;
 
+import com.sun.appserv.connectors.internal.api.ConnectorRuntime;
 import com.sun.appserv.security.AppservRealm;
 import static com.sun.enterprise.security.BaseRealm.JAAS_CONTEXT_PARAM;
 import com.sun.enterprise.security.auth.realm.BadRealmException;
-import com.sun.enterprise.security.auth.realm.InvalidOperationException;
 import com.sun.enterprise.security.auth.realm.NoSuchRealmException;
-import com.sun.enterprise.security.auth.realm.NoSuchUserException;
+import com.sun.enterprise.security.common.Util;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
-import java.util.StringTokenizer;
 import java.util.Vector;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.glassfish.hk2.api.ActiveDescriptor;
+import org.glassfish.hk2.utilities.BuilderHelper;
 
 /**
  *
@@ -24,15 +38,40 @@ public class SaltRealm extends AppservRealm {
     public static final String CHARSET = "charset";
     public static final String GROUPS_SQL = "groups-sql";
 
-    @Override
-    public synchronized void init(Properties properties) throws BadRealmException, NoSuchRealmException {
-        setProperty(JAAS_CONTEXT_PARAM, properties.getProperty(JAAS_CONTEXT_PARAM));
-        setProperty(PASSWORD_SQL_QUERY, properties.getProperty(PASSWORD_SQL_QUERY));
-        setProperty(GROUPS_SQL_QUERY, properties.getProperty(GROUPS_SQL_QUERY));
-        setProperty(JTA_DATA_SOURCE, properties.getProperty(JTA_DATA_SOURCE));
-        setProperty(HASH_ALGORITHM, properties.getProperty(HASH_ALGORITHM));
-        setProperty(CHARSET, properties.getProperty(CHARSET));
-        setProperty(GROUPS_SQL, properties.getProperty(GROUPS_SQL));
+    private static DataSource dataSource;
+
+    private Connection getConnection() {
+        try {
+            synchronized (this) {
+                if (dataSource == null) {
+                    ActiveDescriptor<ConnectorRuntime> cr = (ActiveDescriptor<ConnectorRuntime>) Util.getDefaultHabitat().getBestDescriptor(BuilderHelper.createContractFilter(ConnectorRuntime.class.getName()));
+                    ConnectorRuntime connectorRuntime = Util.getDefaultHabitat().getServiceHandle(cr).getService();
+                    dataSource = (DataSource) connectorRuntime.lookupNonTxResource(getJtaDataSource(), false);
+                }
+            }
+
+            return dataSource.getConnection();
+        } catch (NamingException | SQLException ex) {
+            throw new SaltRealmException(ex);
+        }
+    }
+
+    private void close(ResultSet resultSet, Statement statement, Connection connection) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            throw new SaltRealmException(ex);
+        }
     }
 
     public String getGroupsSql() {
@@ -60,6 +99,16 @@ public class SaltRealm extends AppservRealm {
     }
 
     @Override
+    public synchronized void init(Properties properties) throws BadRealmException, NoSuchRealmException {
+        setProperty(JAAS_CONTEXT_PARAM, properties.getProperty(JAAS_CONTEXT_PARAM));
+        setProperty(PASSWORD_SQL_QUERY, properties.getProperty(PASSWORD_SQL_QUERY));
+        setProperty(GROUPS_SQL_QUERY, properties.getProperty(GROUPS_SQL_QUERY));
+        setProperty(JTA_DATA_SOURCE, properties.getProperty(JTA_DATA_SOURCE));
+        setProperty(HASH_ALGORITHM, properties.getProperty(HASH_ALGORITHM));
+        setProperty(CHARSET, properties.getProperty(CHARSET));
+    }
+
+    @Override
     public String getAuthType() {
         return "jdbc";
     }
@@ -69,17 +118,82 @@ public class SaltRealm extends AppservRealm {
         return "saltRealm";
     }
 
+    public boolean authenticateUser(String username, String password) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        boolean result = true;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(getPasswordQuery());
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String passwd = rs.getString(1);
+                String salt = rs.getString(2);
+
+                if (passwd == null || !passwd.equals(getHash(salt, password))) {
+                    result = false;
+                }
+            }
+        } catch (SQLException ex) {
+            throw new SaltRealmException(ex);
+        } finally {
+            close(rs, stmt, conn);
+        }
+
+        return result;
+    }
+
+    public String getHash(String salt, String password) {
+        try {
+            String passwd = salt + password;
+            MessageDigest digest = MessageDigest.getInstance(getHashAlgorithm());
+            digest.update(passwd.getBytes(Charset.forName(getCharset())));
+            return Base64.getEncoder().encodeToString(digest.digest());
+        } catch (NoSuchAlgorithmException ex) {
+            throw new SaltRealmException(ex);
+        }
+    }
+    
     @Override
     @SuppressWarnings("UseOfObsoleteCollectionType")
-    public Enumeration getGroupNames(String username) throws InvalidOperationException, NoSuchUserException {
+    public Enumeration getGroupNames(String username) {
         Vector<String> vector = new Vector<String>();
-        StringTokenizer tokenizer = new StringTokenizer(getGroupsSql());
+        List<String> groups = getGroupList(username);
 
-        while (tokenizer.hasMoreTokens()) {
-            String str = (String) tokenizer.nextElement();
-            vector.add(str);
+        for (String group : groups) {
+            vector.add(group);
         }
-        
+
         return vector.elements();
     }
+
+    public List<String> getGroupList(String username) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<String> groups = new ArrayList<>();
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(getGroupsQuery());
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String group = rs.getString(1);
+                groups.add(group);
+            }
+        } catch (SQLException ex) {
+            throw new SaltRealmException(ex);
+        } finally {
+            close(rs, stmt, conn);
+        }
+
+        return groups;
+    }
+
 }
